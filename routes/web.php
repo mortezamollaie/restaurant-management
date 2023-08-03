af472ae2650e4fd7c2e14764abd34731e93fedea
morteza <mm.gov.1381@gmail.com>
2023-08-03 15:26:22 +0330

    add controller and fix UI part

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,12 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\MenuController;
+use App\Http\Controllers\Admin\TableController;
+use App\Http\Controllers\Admin\ReservationController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -32,6 +25,14 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function(){
     Route::get('/',[AdminController::class, 'index'])->name('index');
+
+    Route::resource('/categories', CategoryController::class);
+
+    Route::resource('/menus', MenuController::Class);
+
+    Route::resource('/tables', TableController::class);
+
+    Route::resource('/reservation', ReservationController::class);
 });
 
 require __DIR__.'/auth.php';
