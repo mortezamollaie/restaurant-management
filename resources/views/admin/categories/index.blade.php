aae462e4b74fae2008500cdb30de1919ad23c4bf
morteza <mm.gov.1381@gmail.com>
2023-08-05 20:36:46 +0330

    develop update and delete category page

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -26,18 +26,32 @@
                         @foreach ($categories as $category)
                             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                 <td
-                                    class="px-6 py-4
-                                text-sm font-medium text-gray-900 whitespace-nowrapdark:text-white">
-                                    {{ $category->name }}</td>
+                                    class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrapdark:text-white">
+                                    {{ $category->name }}
+                                </td>
                                 <td
-                                    class="px-6 py-4
-                                text-sm font-medium text-gray-900 whitespace-nowrapdark:text-white">
+                                    class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrapdark:text-white">
                                     <img src="{{ Storage::url($category->image) }}" class="w-16 h-16 rounded">
                                 </td>
                                 <td
-                                    class="px-6 py-4
-                                text-sm font-medium text-gray-900 whitespace-nowrapdark:text-white">
-                                    {{ $category->description }}</td>
+                                    class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrapdark:text-white">
+                                    {{ $category->description }}
+                                </td>
+                                <td
+                                    class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrapdark:text-white">
+                                    <div class="flex space-x-2">
+                                        <a href="{{ route('admin.categories.edit', $category->id) }}"
+                                            class="px-4 py-2 bg-green-500 hover:bg-green-700 rounded-lg text-white">Edit</a>
+                                        <form class="px-4 py-2 bg-red-500 hover:bg-red-700 rounded-lg text-white"
+                                            method="POST"
+                                            action="{{ route('admin.categories.destroy', $category->id) }}"
+                                            onsubmit="return confirm('Are you sure?');">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit">Delete</button>
+                                        </form>
+                                    </div>
+                                </td>
                         @endforeach
                         </tr>
 
